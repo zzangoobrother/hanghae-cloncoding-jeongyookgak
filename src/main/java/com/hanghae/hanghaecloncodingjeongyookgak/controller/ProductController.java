af4d2d19438ff77da066739132eaac43d757d3a3
zzangoobrother <com1368@naver.com>
2021-10-19 13:38:51 +0900

    상품 리스트 조회, 단건 조회 리팩토링

--- src/main/java/com/hanghae/hanghaecloncodingjeongyookgak/controller/ProductController.java
+++ src/main/java/com/hanghae/hanghaecloncodingjeongyookgak/controller/ProductController.java
@@ -20,30 +20,13 @@ public class ProductController {
     // 쇼핑하기 카테고리 조회
     @GetMapping("/api/list")
     public Map<String, Object> getCategoryProducts(@RequestParam String category) {
-        List<Product> products = productService.getCategoryProducts(category);
-
-        Map<String, Object> result = new HashMap<>();
-        result.put("products", products);
-        result.put("result", "success");
-
-        return result;
+        return productService.getCategoryProducts(category);
     }
 
     // 상품 단건 조회
     @GetMapping("/api/detail")
     public Map<String, String> getProduct(@RequestParam Long productId) {
-        Product product = productService.getProduct(productId);
-
-        Map<String, String> result = new HashMap<>();
-        result.put("id", String.valueOf(product.getId()));
-        result.put("category", product.getCategory());
-        result.put("title", product.getTitle());
-        result.put("price", String.valueOf(product.getPrice()));
-        result.put("image", product.getImage());
-        result.put("imageDetail", product.getImageDetail());
-        result.put("result", "success");
-
-        return result;
+        return productService.getProduct(productId);
     }
 
     // 상품 등록위해 구현
--- src/main/java/com/hanghae/hanghaecloncodingjeongyookgak/service/ProductService.java
+++ src/main/java/com/hanghae/hanghaecloncodingjeongyookgak/service/ProductService.java
@@ -6,7 +6,9 @@ import com.hanghae.hanghaecloncodingjeongyookgak.model.Product;
 import com.hanghae.hanghaecloncodingjeongyookgak.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProductService {
@@ -21,14 +23,30 @@ public class ProductService {
         return productRepository.findAllByCategory("PORK");
     }
 
-    public List<Product> getCategoryProducts(String category) {
-        return productRepository.findAllByCategory(category);
+    public Map<String, Object> getCategoryProducts(String category) {
+        List<Product> products = productRepository.findAllByCategory(category);
+        Map<String, Object> result = new HashMap<>();
+        result.put("products", products);
+        result.put("result", "success");
+
+        return result;
     }
 
-    public Product getProduct(Long productId) {
-        return productRepository.findById(productId).orElseThrow(
+    public Map<String, String> getProduct(Long productId) {
+        Product product = productRepository.findById(productId).orElseThrow(
                 () -> new HanghaeClonException(ErrorCode.PRODUCT_NOT_FOUND)
         );
+
+        Map<String, String> result = new HashMap<>();
+        result.put("id", String.valueOf(product.getId()));
+        result.put("category", product.getCategory());
+        result.put("title", product.getTitle());
+        result.put("price", String.valueOf(product.getPrice()));
+        result.put("image", product.getImage());
+        result.put("imageDetail", product.getImageDetail());
+        result.put("result", "success");
+
+        return result;
     }
 
     public Product createProduct(Product product) {
