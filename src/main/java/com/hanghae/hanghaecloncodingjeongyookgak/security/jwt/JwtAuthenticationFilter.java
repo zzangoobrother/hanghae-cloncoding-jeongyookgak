61d6a229a049073cb77bfee2ed9abe14dfd0ade6
zzangoobrother <com1368@naver.com>
2021-10-21 00:09:54 +0900

    FIX jwt Authorization 수정

--- src/main/java/com/hanghae/hanghaecloncodingjeongyookgak/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/hanghae/hanghaecloncodingjeongyookgak/security/jwt/JwtAuthenticationFilter.java
@@ -21,7 +21,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         //헤더에서 jwt 토큰 받아옴
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request).replaceAll("Bearer", "");
 
         if (token != null && jwtTokenProvider.validateToken(token)){
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
